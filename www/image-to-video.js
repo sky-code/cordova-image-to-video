538d0b7201242e66e9025396201048e9dc50c5b9
Sky <skycodes@gmail.com>
2016-02-03 16:22:56 +0200

    update CDVImageToVideo

--- www/image-to-video.js
+++ www/image-to-video.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.sayHello = function(success, error, arg0) {
-    exec(success, error, "ImageToVideo", "sayHello", [arg0]);
+exports.sayHello = function (success, error, frames, width, height, frameRate) {
+    exec(success, error, "ImageToVideo", "sayHello", [frames, width, height, frameRate]);
 };
