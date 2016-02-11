aabb951610ad212527a8ed7b8e0a311faf0cba65
Sky <skycodes@gmail.com>
2016-02-11 19:32:12 +0200

    changes for debuging js call to plugin

--- www/image-to-video.js
+++ www/image-to-video.js
@@ -1,12 +1,12 @@
 var exec = require('cordova/exec');
 
-exports.sayHello = function (success, error, frames, width, height, fps) {
-    var params = frames.slice();
+exports.sayHello = function (success, error, params/*frames, width, height, fps*/) {
+    /*var params = frames.slice();
     var options = {
         width: width,
         height: height,
         fps: fps
     };
-    params.unshift(options);
+    params.unshift(options);*/
     exec(success, error, "ImageToVideo", "sayHello", params);
 };
