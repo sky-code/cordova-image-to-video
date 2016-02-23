81c201a6e602478dbf162943283ff9db7caeaf1e
Sky <skycodes@gmail.com>
2016-02-23 16:05:29 +0200

    update image-to-video.js args processing, add docs

--- www/image-to-video.js
+++ www/image-to-video.js
@@ -1,12 +1,18 @@
 var exec = require('cordova/exec');
 
-exports.convert = function (success, error, params/*frames, width, height, fps*/) {
-    /*var params = frames.slice();
-    var options = {
-        width: width,
-        height: height,
-        fps: fps
-    };
-    params.unshift(options);*/
-    exec(success, error, "ImageToVideo", "convert", params);
+/**
+ * convert array of frames into video
+ * @param {function} successCallback - success callback function
+ * @param {function} errorCallback - error callback function
+ * @param {object} options - set of options
+ * @param {string} options.videoFilePath - path for result video file, can be relative or full path url
+ * @param {number} options.width - video width
+ * @param {number} options.height - video height
+ * @param {number} options.fps - video fps
+ * @param {string[]} frames - array of base64 encoded images
+ */
+exports.convert = function (successCallback, errorCallback, options, frames) {
+    var args = Array.from(frames);
+    args.unshift(options);
+    exec(successCallback, errorCallback, "ImageToVideo", "convert", args);
 };
