7e701a4998cfa87f7669f5cc71c00ac6901403c5
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-2.local>
2016-06-07 17:07:24 +0300

    cleanup, remove old assetWriterInputAppendUIImage method, fix bug with outputFilePath url

--- src/ios/CDVImageToVideo.swift
+++ src/ios/CDVImageToVideo.swift
@@ -16,16 +16,25 @@ class CDVImageToVideo: CDVPlugin {
             let frame = command.argumentAtIndex(UInt(i)) as! String
             frames.append(frame)
         }
+        
 
-        let outputFileURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("instagram.mp4")
-        //let outputFileURL = NSURL(fileURLWithPath: filePath)
+        var outputFileURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
+            .URLByAppendingPathComponent("instagram.mp4")
+        
+        if !filePath.isEmpty {
+            outputFileURL = NSURL(string: filePath)!
+            if ((outputFileURL.fileURL) == false){
+                outputFileURL = NSURL(fileURLWithPath: filePath)
+            }
+        }
+        
 		self.commandDelegate!.runInBackground({
         let fileManager = NSFileManager.defaultManager()
         if fileManager.fileExistsAtPath(outputFileURL.path!) {
             do {
                 try fileManager.removeItemAtPath(outputFileURL.path!)
             } catch let error as NSError {
-                fatalError("Unable to delete file: \(error.localizedDescription) : \(__FUNCTION__).")
+                fatalError("Unable to delete file: \(error.localizedDescription) : \(#function).")
             }
         }
 
@@ -77,11 +86,11 @@ class CDVImageToVideo: CDVPlugin {
 
             let image = self.UIImageFromBase64DataURL(frame)
 
-            if !self.waitForAVAssetWriterInput(adaptor.assetWriterInput, retryingAttempt: 0) {
+            if !self.waitForAVAssetWriterInput(adaptor.assetWriterInput, retryingAttempt: 1) {
                 NSLog("assetWriterInput.readyForMoreMediaData always false")
             }
 
-            let pixelBufferAppend = self.assetWriterInputAppendUIImage2(adaptor, image: image, presentationTime: presentTime)
+            let pixelBufferAppend = self.assetWriterInputAppendUIImage(adaptor, image: image, presentationTime: presentTime)
 
             if !pixelBufferAppend {
                 NSLog("appendPixelBuffer \(index) error");
@@ -114,15 +123,7 @@ class CDVImageToVideo: CDVPlugin {
         }
     }
 
-    private func assetWriterInputAppendUIImage(adaptor: AVAssetWriterInputPixelBufferAdaptor, image: UIImage, presentationTime: CMTime) -> Bool {
-        let CGImg = image.CGImage!
-        let frameSize = CGSizeMake(CGFloat(CGImageGetWidth(CGImg)), CGFloat(CGImageGetHeight(CGImg)))
-        let buffer: CVPixelBufferRef = self.pixelBufferFromCGImage(CGImg, frameSize: frameSize)
-        let pixelBufferAppend = adaptor.appendPixelBuffer(buffer, withPresentationTime: presentationTime)
-        return pixelBufferAppend
-    }
-
-    private func assetWriterInputAppendUIImage2(pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor, image: UIImage, presentationTime: CMTime) -> Bool {
+    private func assetWriterInputAppendUIImage(pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor, image: UIImage, presentationTime: CMTime) -> Bool {
         var appendSucceeded = true
         autoreleasepool {
             // var pixelBuffer: Unmanaged<CVPixelBuffer>?
@@ -146,71 +147,33 @@ class CDVImageToVideo: CDVPlugin {
         return appendSucceeded
     }
 
-    private func waitForAVAssetWriterInput(assetWriterInput: AVAssetWriterInput, var retryingAttempt: Int) -> Bool {
+    private func waitForAVAssetWriterInput(assetWriterInput: AVAssetWriterInput, retryingAttempt: Int) -> Bool {
         if (assetWriterInput.readyForMoreMediaData) {
             return true;
         } else {
-            retryingAttempt = retryingAttempt + 1
             if retryingAttempt > 30 {
                 NSLog("Error: Adaptor is not ready, at 30 retrying attempts")
                 return false;
             }
             NSLog("Error: Adaptor is not ready, retryingAttempt: \(retryingAttempt)")
             NSThread.sleepForTimeInterval(0.2)
-            return self.waitForAVAssetWriterInput(assetWriterInput, retryingAttempt: retryingAttempt)
+            return self.waitForAVAssetWriterInput(assetWriterInput, retryingAttempt: (retryingAttempt + 1))
         }
     }
 
-    private func pixelBufferFromCGImage(img: CGImageRef, frameSize: CGSize) -> CVPixelBufferRef {
-        // OLD METHOD
-        let options = [
-
-                "kCVPixelBufferCGImageCompatibilityKey": true,
-
-                "kCVPixelBufferCGBitmapContextCompatibilityKey": true
-
-        ]
-
-        let pixelBufferPointer = UnsafeMutablePointer<CVPixelBuffer?>.alloc(1)
-
-        CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), OSType(kCVPixelFormatType_32ARGB), options, pixelBufferPointer)
-
-        let lockBaseAddress = CVPixelBufferLockBaseAddress(pixelBufferPointer.memory!, 0)
-
-        let pixelData: UnsafeMutablePointer<(Void)> = CVPixelBufferGetBaseAddress(pixelBufferPointer.memory!)
-
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.NoneSkipFirst.rawValue)
-
-        let space: CGColorSpace = CGColorSpaceCreateDeviceRGB()!
-
-        var altCVPixelBufferGetBytesPerRow = 4 * CGImageGetWidth(img);
-        var recommend = CVPixelBufferGetBytesPerRow(pixelBufferPointer.memory!);
-
-        let context: CGContextRef = CGBitmapContextCreate(pixelData, Int(frameSize.width), Int(frameSize.height), 8, CVPixelBufferGetBytesPerRow(pixelBufferPointer.memory!), space, CGImageAlphaInfo.PremultipliedFirst.rawValue)!
-
-        CGContextDrawImage(context, CGRectMake(0, 0, frameSize.width, frameSize.height), img)
-
-        CVPixelBufferUnlockBaseAddress(pixelBufferPointer.memory!, 0)
-
-        return pixelBufferPointer.memory!
-    }
-
     private func fillPixelBufferFromImage(image: UIImage, pixelBuffer: CVPixelBufferRef) {
-        let lockStatus = CVPixelBufferLockBaseAddress(pixelBuffer, 0)
+        CVPixelBufferLockBaseAddress(pixelBuffer, 0)
 
         let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedFirst.rawValue)
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-
-        var altCVPixelBufferGetBytesPerRow = 4 * CGImageGetWidth(image.CGImage);
-        var recommend = CVPixelBufferGetBytesPerRow(pixelBuffer);
 
         let context = CGBitmapContextCreate(
         pixelData,
                 Int(image.size.width),
                 Int(image.size.height),
                 8,
-                Int(4 * image.size.width),
+                Int(4 * image.size.width), // or use CVPixelBufferGetBytesPerRow(pixelBuffer)
                 rgbColorSpace,
                 bitmapInfo.rawValue
         )
@@ -271,74 +234,6 @@ class CDVImageToVideo: CDVPlugin {
             NSLog("unable create UIImage from dataNSData");
         }
         return image!;
-    }
-
-    private func UIImageArrayFromBase64Frames(frames: [String]) -> [UIImage] {
-        NSLog("exec: UIImageArrayFromBase64Frames");
-        var images = [UIImage]();
-        // var images = [UIImage](count: frames.count, repeatedValue: nil);
-
-        for (index, frame) in frames.enumerate() {
-            // var frame:String = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD///+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4Ug9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC";
-            let dataUrl = NSURL(string: frame);
-            if dataUrl == nil {
-                NSLog("dataUrl for frame \(index) is nil");
-                continue
-            }
-            let data = NSData(contentsOfURL: dataUrl!);
-            if data == nil {
-                NSLog("data for frame \(index) is nil");
-                continue
-            }
-            let image = UIImage(data: data!);
-            if image == nil {
-                NSLog("image for frame \(index) is nil");
-                continue;
-            }
-            images.append(image!);
-        }
-        return images;
-    }
-
-    private func pixelBufferFromImage(image: UIImage) -> CVPixelBufferRef {
-        // looks pretty good
-        let size = image.size
-
-        var pixelBuffer: CVPixelBuffer?
-        let bufferOptions: CFDictionary = [
-                "kCVPixelBufferCGBitmapContextCompatibilityKey": NSNumber(bool: true),
-                "kCVPixelBufferCGImageCompatibilityKey": NSNumber(bool: true)
-        ]
-        CVPixelBufferCreate(
-        nil,
-                Int(size.width),
-                Int(size.height),
-                OSType(kCVPixelFormatType_32ARGB),
-                bufferOptions,
-                &pixelBuffer
-        )
-
-        let managedPixelBuffer = pixelBuffer
-
-        CVPixelBufferLockBaseAddress(managedPixelBuffer!, 0)
-
-        let pixelData = CVPixelBufferGetBaseAddress(managedPixelBuffer!)
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedFirst.rawValue)
-        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-        let context = CGBitmapContextCreate(
-        pixelData,
-                Int(size.width),
-                Int(size.height),
-                8,
-                Int(4 * size.width),
-                rgbColorSpace,
-                bitmapInfo.rawValue
-        )
-        CGContextDrawImage(context, CGRectMake(0, 0, size.width, size.height), image.CGImage)
-
-        CVPixelBufferUnlockBaseAddress(managedPixelBuffer!, 0)
-
-        return managedPixelBuffer!
     }
 
     // lifecycle logs
