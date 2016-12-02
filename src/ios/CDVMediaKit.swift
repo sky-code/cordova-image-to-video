b483d5edc694c38a4d9d2d34db5890eb9c53cb29
sky-code <skycodes@gmail.com>
2016-12-02 23:21:14 +0200

    update to swift 3

--- src/ios/CDVMediaKit.swift
+++ src/ios/CDVMediaKit.swift
@@ -1,47 +1,48 @@
 import Foundation
 import AVFoundation
 import Photos
+// swift 3
 
 @objc(CDVMediaKit)
 class CDVMediaKit: CDVPlugin {
     
-    func saveVideoToPhotoLibrary(command: CDVInvokedUrlCommand) {
+    func saveVideoToPhotoLibrary(_ command: CDVInvokedUrlCommand) {
         NSLog("CDVMediaKit#saveVideoToPhotoLibrary()")
-        let filePath = String(command.argumentAtIndex(0) as! NSString)
+        let filePath = String(command.argument(at: 0) as! NSString)
         NSLog(filePath)
         
         if filePath.isEmpty{
             NSLog("error filePath is empty")
         }
         
-        var fileURL = NSURL(string: filePath)!
+        var fileURL = URL(string: filePath)!
         
-        if !fileURL.fileURL {
-            fileURL = NSURL(fileURLWithPath: filePath)
+        if !fileURL.isFileURL {
+            fileURL = URL(fileURLWithPath: filePath)
         }
         
         var localIdentifier:String? = nil
-        PHPhotoLibrary.sharedPhotoLibrary().performChanges({ () -> Void in
+        PHPhotoLibrary.shared().performChanges({ () -> Void in
             
-            let createAssetRequest: PHAssetChangeRequest = PHAssetChangeRequest.creationRequestForAssetFromVideoAtFileURL(fileURL)!
+            let createAssetRequest: PHAssetChangeRequest = PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: fileURL)!
             let placeholder = createAssetRequest.placeholderForCreatedAsset
             localIdentifier = placeholder?.localIdentifier
         }, completionHandler: { (success, error) -> Void in
             if success {
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: localIdentifier);
-                self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId);
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: localIdentifier);
+                self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
             }
             else {
-                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAsString: error?.localizedDescription);
-                self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId);
+                let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: error?.localizedDescription);
+                self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
             }
         })
         
     }
     
-    func convert(command: CDVInvokedUrlCommand) {
+    func convert(_ command: CDVInvokedUrlCommand) {
         NSLog("CDVMediaKit#convert()")
-        let options = command.argumentAtIndex(0) as! NSDictionary
+        let options = command.argument(at: 0) as! NSDictionary
         let filePath = String(options["filePath"] as! NSString)
         let width = Int(options["width"] as! NSNumber)
         let height = Int(options["height"] as! NSNumber)
@@ -49,26 +50,26 @@ class CDVMediaKit: CDVPlugin {
 
         var frames = [String]()
         for i in 1 ... (command.arguments.count - 1) {
-            let frame = command.argumentAtIndex(UInt(i)) as! String
+            let frame = command.argument(at: UInt(i)) as! String
             frames.append(frame)
         }
         
 
-        var outputFileURL = NSURL(fileURLWithPath: NSTemporaryDirectory())
-            .URLByAppendingPathComponent("instagram.mp4")
+        var outputFileURL = URL(fileURLWithPath: NSTemporaryDirectory())
+            .appendingPathComponent("instagram.mp4")
         
         if !filePath.isEmpty {
-            outputFileURL = NSURL(string: filePath)!
-            if ((outputFileURL.fileURL) == false){
-                outputFileURL = NSURL(fileURLWithPath: filePath)
+            outputFileURL = URL(string: filePath)!
+            if ((outputFileURL.isFileURL) == false){
+                outputFileURL = URL(fileURLWithPath: filePath)
             }
         }
         
-		self.commandDelegate!.runInBackground({
-        let fileManager = NSFileManager.defaultManager()
-        if fileManager.fileExistsAtPath(outputFileURL.path!) {
+		self.commandDelegate!.run(inBackground: {
+        let fileManager = FileManager.default
+        if fileManager.fileExists(atPath: outputFileURL.path) {
             do {
-                try fileManager.removeItemAtPath(outputFileURL.path!)
+                try fileManager.removeItem(atPath: outputFileURL.path)
             } catch let error as NSError {
                 fatalError("Unable to delete file: \(error.localizedDescription) : \(#function).")
             }
@@ -77,17 +78,17 @@ class CDVMediaKit: CDVPlugin {
         self.startConverting(outputFileURL, frames: frames, width: width, height: height, frameRate: fps);
         NSLog("outputFileURL: \(outputFileURL)")
 
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAsString: outputFileURL.path);
-        self.commandDelegate!.sendPluginResult(pluginResult, callbackId: command.callbackId);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: outputFileURL.path);
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
 		})
     }
 
-    private func startConverting(outputFileURL: NSURL, frames: [String], width: Int, height: Int, frameRate: Int) {
+    fileprivate func startConverting(_ outputFileURL: URL, frames: [String], width: Int, height: Int, frameRate: Int) {
         // prepare complete
 
         let videoWriter: AVAssetWriter!
         do {
-            videoWriter = try AVAssetWriter(URL: outputFileURL, fileType: AVFileTypeMPEG4)
+            videoWriter = try AVAssetWriter(outputURL: outputFileURL, fileType: AVFileTypeMPEG4)
         } catch let error as NSError {
             NSLog("Create AVAssetWriter error: \(error.localizedDescription)");
             videoWriter = nil
@@ -96,22 +97,22 @@ class CDVMediaKit: CDVPlugin {
         let assetWriterInput = self.createAVAssetWriterInput(width, height: height);
 
 
-        let adaptorAttributes: [String:AnyObject] = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB),
-                                                     kCVPixelBufferWidthKey as String: Float(width),
-                                                     kCVPixelBufferHeightKey as String: Float(height)]
+        let adaptorAttributes: [String:AnyObject] = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32ARGB) as AnyObject,
+                                                     kCVPixelBufferWidthKey as String: Float(width) as AnyObject,
+                                                     kCVPixelBufferHeightKey as String: Float(height) as AnyObject]
 
         let adaptor = AVAssetWriterInputPixelBufferAdaptor(assetWriterInput: assetWriterInput,
                 sourcePixelBufferAttributes: adaptorAttributes)
 
-        videoWriter.addInput(assetWriterInput)
+        videoWriter.add(assetWriterInput)
 
         videoWriter.startWriting()
 
-        videoWriter.startSessionAtSourceTime(kCMTimeZero)
+        videoWriter.startSession(atSourceTime: kCMTimeZero)
 
         // var buffer: CVPixelBufferRef
 
-        for (index, frame) in frames.enumerate() {
+        for (index, frame) in frames.enumerated() {
             let fps: Int32 = Int32(frameRate)
 
             let frameTime = CMTimeMake(1, fps)
@@ -135,12 +136,12 @@ class CDVMediaKit: CDVPlugin {
         }
         assetWriterInput.markAsFinished()
 
-        videoWriter.finishWritingWithCompletionHandler {
+        videoWriter.finishWriting {
             () -> Void in
 
             switch videoWriter.status {
 
-            case AVAssetWriterStatus.Failed:
+            case AVAssetWriterStatus.failed:
 
                 NSLog("Error: \(videoWriter.error)")
 
@@ -159,7 +160,7 @@ class CDVMediaKit: CDVPlugin {
         }
     }
 
-    private func assetWriterInputAppendUIImage(pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor, image: UIImage, presentationTime: CMTime) -> Bool {
+    fileprivate func assetWriterInputAppendUIImage(_ pixelBufferAdaptor: AVAssetWriterInputPixelBufferAdaptor, image: UIImage, presentationTime: CMTime) -> Bool {
         var appendSucceeded = true
         autoreleasepool {
             // var pixelBuffer: Unmanaged<CVPixelBuffer>?
@@ -176,15 +177,15 @@ class CDVMediaKit: CDVPlugin {
             }
             let managedPixelBuffer = pixelBuffer!
             self.fillPixelBufferFromImage(image, pixelBuffer: managedPixelBuffer)
-            appendSucceeded = pixelBufferAdaptor.appendPixelBuffer(managedPixelBuffer,
+            appendSucceeded = pixelBufferAdaptor.append(managedPixelBuffer,
                     withPresentationTime: presentationTime)
         }
 
         return appendSucceeded
     }
 
-    private func waitForAVAssetWriterInput(assetWriterInput: AVAssetWriterInput, retryingAttempt: Int) -> Bool {
-        if (assetWriterInput.readyForMoreMediaData) {
+    fileprivate func waitForAVAssetWriterInput(_ assetWriterInput: AVAssetWriterInput, retryingAttempt: Int) -> Bool {
+        if (assetWriterInput.isReadyForMoreMediaData) {
             return true;
         } else {
             if retryingAttempt > 30 {
@@ -192,34 +193,34 @@ class CDVMediaKit: CDVPlugin {
                 return false;
             }
             NSLog("Error: Adaptor is not ready, retryingAttempt: \(retryingAttempt)")
-            NSThread.sleepForTimeInterval(0.2)
+            Thread.sleep(forTimeInterval: 0.2)
             return self.waitForAVAssetWriterInput(assetWriterInput, retryingAttempt: (retryingAttempt + 1))
         }
     }
 
-    private func fillPixelBufferFromImage(image: UIImage, pixelBuffer: CVPixelBufferRef) {
-        CVPixelBufferLockBaseAddress(pixelBuffer, 0)
+    fileprivate func fillPixelBufferFromImage(_ image: UIImage, pixelBuffer: CVPixelBuffer) {
+        CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
 
         let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer)
-        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedFirst.rawValue)
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
         let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
 
-        let context = CGBitmapContextCreate(
-        pixelData,
-                Int(image.size.width),
-                Int(image.size.height),
-                8,
-                Int(4 * image.size.width), // or use CVPixelBufferGetBytesPerRow(pixelBuffer)
-                rgbColorSpace,
-                bitmapInfo.rawValue
+        let context = CGContext(
+        data: pixelData,
+                width: Int(image.size.width),
+                height: Int(image.size.height),
+                bitsPerComponent: 8,
+                bytesPerRow: Int(4 * image.size.width), // or use CVPixelBufferGetBytesPerRow(pixelBuffer)
+                space: rgbColorSpace,
+                bitmapInfo: bitmapInfo.rawValue
         )
 
-        CGContextDrawImage(context, CGRectMake(0, 0, image.size.width, image.size.height), image.CGImage)
+        context?.draw(image.cgImage!, in: CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height))
 
-        CVPixelBufferUnlockBaseAddress(pixelBuffer, 0)
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
     }
 
-    private func createAVAssetWriterInput(width: Int, height: Int) -> AVAssetWriterInput {
+    fileprivate func createAVAssetWriterInput(_ width: Int, height: Int) -> AVAssetWriterInput {
         let videoCleanApertureSettings = [AVVideoCleanApertureWidthKey: width,
 
                                           AVVideoCleanApertureHeightKey: height,
@@ -248,20 +249,20 @@ class CDVMediaKit: CDVPlugin {
 
                              AVVideoWidthKey: width,
 
-                             AVVideoHeightKey: height]
+                             AVVideoHeightKey: height] as [String : Any]
 
         let assetWriterInput = AVAssetWriterInput(mediaType: AVMediaTypeVideo, outputSettings: videoSettings as? [String:AnyObject])
         assetWriterInput.expectsMediaDataInRealTime = true
         return assetWriterInput
     }
 
-    private func UIImageFromBase64DataURL(DataURL: String) -> UIImage {
+    fileprivate func UIImageFromBase64DataURL(_ DataURL: String) -> UIImage {
         // let DataURL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0iAAAABlBMVEUAAAD///+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83NDDeNGe4Ug9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC";
-        let dataNSURL = NSURL(string: DataURL);
+        let dataNSURL = URL(string: DataURL);
         if dataNSURL == nil {
             NSLog("unable create NSURL from DataURL");
         }
-        let dataNSData = NSData(contentsOfURL: dataNSURL!);
+        let dataNSData = try? Data(contentsOf: dataNSURL!);
         if dataNSData == nil {
             NSLog("unable create NSData from dataNSURL");
         }
